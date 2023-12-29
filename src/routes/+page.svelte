77c0c5385167dad5de808f4a81704cabfa8b6227
Michée Allidjinou <allidjinoumichee@outlook.fr>
2023-12-29 08:41:45 +0000

    feat: add job components as example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+	import JobStatus from '$lib/jobStatus';
+	import JobComponent from './JobComponent.svelte';
+</script>
+
+<div class="flex flex-col my-4 gap-2">
+	<JobComponent jobTitle="Echo" jobStatus={JobStatus.Completed} />
+	<JobComponent jobTitle="Clean log files" jobStatus={JobStatus.InProgress} />
+
+	<JobComponent
+		jobTitle="Download reference files"
+		jobStatus={JobStatus.Failed}
+		jobErrorDetails="Server returned 403"
+	/>
+</div>
 
 <style lang="postcss">
 	:global(html) {
