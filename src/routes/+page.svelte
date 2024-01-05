c3b74495f53b2722b87000260659b92f5e2ccc39
Michée Allidjinou <allidjinoumichee@outlook.fr>
2024-01-05 15:09:26 +0100

    save current changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,38 @@
-<script>
+<script lang="ts">
 	import JobStatus from '$lib/jobStatus';
+	import { onMount } from 'svelte';
 	import JobComponent from './JobComponent.svelte';
+
+	let messages: string[] = [];
+	// let socket;
+
+	const addMessage = (message: string) => {
+		messages = [...messages, message];
+	};
+
+	onMount(() => {
+		const socket = new WebSocket('ws://localhost:8080/ws');
+
+		socket.addEventListener('open', function (event) {
+			console.log('websocket connection opened.');
+		});
+
+		socket.addEventListener('message', function (event) {
+			addMessage(event.data);
+			console.log('received message:', event.data);
+		});
+
+		return () => {
+			console.log('websocket connection closed.');
+			socket.close();
+		};
+	});
 </script>
 
 <div class="flex flex-col my-4 gap-2">
+	{#each messages as message}
+		<JobComponent jobTitle={message} jobStatus={JobStatus.Completed} />
+	{/each}
 	<JobComponent jobTitle="Echo" jobStatus={JobStatus.Completed} />
 	<JobComponent jobTitle="Clean log files" jobStatus={JobStatus.InProgress} />
 
